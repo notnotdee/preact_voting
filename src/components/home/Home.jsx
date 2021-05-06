a7ab52a33d4a2b4a40c34e362fb50426189126e0
dl-watson <dlarkinwatson@gmail.com>
2021-05-06 09:49:12 -0700

    setting up basic input/output view

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,5 +1,24 @@
-import React from 'react';
+import { h } from 'preact';
+import { useState } from 'preact/hooks';
 
 export const Home = () => {
-  return <div>Hello World!</div>;
+  const [input, setInput] = useState('');
+  const [options, setOptions] = useState([]);
+
+  const handleSend = () => {
+    setOptions((prev) => [...prev, input]);
+  };
+
+  const optionsList = options.map((elem) => {
+    return <li>{elem}</li>;
+  });
+
+  return (
+    <div>
+      <h1>Voting</h1>
+      <input onChange={(e) => setInput(e.target.value)} />
+      <button onClick={handleSend}>Send</button>
+      <ul>{optionsList}</ul>
+    </div>
+  );
 };
