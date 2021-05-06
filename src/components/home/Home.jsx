967af2988576e0b1b91bdb45a0beeb56536f8c3a
dl-watson <dlarkinwatson@gmail.com>
2021-05-06 12:33:08 -0700

    drag and drop api

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -4,19 +4,65 @@ import { useState } from 'preact/hooks';
 export const Home = () => {
   const [input, setInput] = useState('');
   const [options, setOptions] = useState([]);
+  const [dragId, setDragId] = useState();
 
   const handleSend = () => {
-    setOptions((prev) => [...prev, input]);
+    setOptions((prev) => {
+      let len = prev.length;
+      return [
+        ...prev,
+        {
+          id: prev[len - 1]?.id + 1 || 0,
+          order: prev[len - 1]?.order + 1 || 0,
+          val: input,
+        },
+      ];
+    });
   };
 
-  const optionsList = options.map((elem) => {
-    return <li>{elem}</li>;
-  });
+  const handleDrag = ({ currentTarget: target }) => {
+    setDragId(target.id);
+  };
+
+  const handleDrop = ({ currentTarget: target }) => {
+    const dragElem = options.find((elem) => elem.id == dragId);
+    const dropElem = options.find((elem) => elem.id == target.id);
+
+    const newElemState = options.map((elem) => {
+      if (elem.id == dragId) {
+        elem = { ...elem, order: dropElem.order };
+      }
+
+      if (elem.id == target.id) {
+        elem = { ...elem, order: dragElem.order };
+      }
+
+      return elem;
+    });
+
+    setOptions(newElemState);
+  };
+
+  const optionsList = options
+    .sort((a, b) => a.order - b.order)
+    .map((elem) => {
+      return (
+        <li
+          id={elem.id}
+          draggable={true}
+          onDragOver={(e) => e.preventDefault()}
+          onDragStart={handleDrag}
+          onDrop={handleDrop}
+        >
+          {elem.val}
+        </li>
+      );
+    });
 
   return (
-    <div>
+    <div class="container">
       <h1>Voting</h1>
-      <input onChange={(e) => setInput(e.target.value)} />
+      <input value={input} onChange={({ target }) => setInput(target.value)} />
       <button onClick={handleSend}>Send</button>
       <ul>{optionsList}</ul>
     </div>
